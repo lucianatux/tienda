1d0dfdb7989623c838f30bfb5a5a8b780fd0ada4
lucianatux <lucianatux@yahoo.com.ar>
2023-12-18 21:16:44 -0300

    listo

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { selectedProduct } from "../redux/actions/productActions";
+import { selectedProduct, removeSelectedProduct } from "../redux/actions/productActions";
 const ProductDetail = () => {
   const product = useSelector((state) => state.product);
   const { image, title, price, category, description } = product;
@@ -22,6 +22,9 @@ const ProductDetail = () => {
   };
   useEffect(() => {
     if (productId && productId !== "") fetchProductDetail();
+    return ()=>{
+      dispatch(removeSelectedProduct());
+    };
   }, [productId]);
   return (
     <div className="container">
@@ -38,7 +41,7 @@ const ProductDetail = () => {
               {/* Columna derecha */}
               <h1>{title}</h1>
               <h2>
-                <a className="btn btn-teal">${price}</a>
+                <div className="tag-label">${price}</div>
               </h2>
               <h3 className="brown block header">{category}</h3>
               <p>{description}</p>
@@ -51,42 +54,6 @@ const ProductDetail = () => {
       )}
     </div>
   );
-
-  {
-    /*
-return (
-    <div className="ui grid container">
-      {Object.keys(product).length === 0 ? (
-        <div>...Loading</div>
-      ) : (
-        <div className="ui placeholder segment">
-          <div className="ui two column stackable center aligned grid">
-            <div className="ui vertival divider">AND</div>
-            <div className="middle aligned row">
-              <div className="column lp"></div>
-              <img className="ui fluid image" src={image} alt={title} />
-            </div>
-            <div className="column rp">
-              <h1>{title}</h1>
-              <h2>
-                <a className="ui teal tag label">${price}</a>
-              </h2>
-              <h3 className="ui brown block header">{category}</h3>
-              <p>{description}</p>
-              <div className="ui vertical animated button" tabIndex="0">
-                <div className="hidden content">
-                  <i className="shop icon"></i>
-                </div>
-                <div className="visible content">Add to cart</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-*/
-  }
 };
 
 export default ProductDetail;
